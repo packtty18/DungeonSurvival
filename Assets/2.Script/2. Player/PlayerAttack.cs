ffbc0f97c3bbb9d31445bb4301d0edb8e043f631
PSH <packtty18@naver.com>
2025-11-17 11:04:36 +0900

    Feat: 플레이어 공격, 스킬 스크립트 생성

---
+++ Assets/2.Script/2. Player/PlayerAttack.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    //플레이어의 조작을 통한 기본 공격
+}
---
+++ Assets/2.Script/2. Player/PlayerSkill.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class PlayerSkill : MonoBehaviour
+{
+    //플레이어의 스킬 획득, 발동 등을 관리함
+    //스킬에는 액티브, 패시브, 엑스트라 존재
+}
