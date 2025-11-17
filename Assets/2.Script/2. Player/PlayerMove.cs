7927ece1997067a42e6cd2530beaf9709ee03adc
PSH <packtty18@naver.com>
2025-11-17 11:02:03 +0900

    Feat : PlayerMove기능 완성

--- Assets/2.Script/2. Player/PlayerMove.cs
+++ Assets/2.Script/2. Player/PlayerMove.cs
@@ -2,17 +2,33 @@
 
 public class PlayerMove : MonoBehaviour, IMovable
 {
-    private float _currentSpeed = 1;
+    [Header("Component")]
+    [SerializeField] private PlayerInput _input;
+
+    [Header("MoveStat")]
+    [SerializeField] private float _currentSpeed = 1;
+    [SerializeField] private Vector2 _currentDirection = Vector2.zero;
     public float CurrentSpeed => _currentSpeed;
 
-    public void Move(Vector2 direction)
+    private void Start()
     {
-        Vector2 newPos = (Vector2)transform.position + direction.normalized * _currentSpeed * Time.deltaTime;
+        _input = GetComponent<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        SetDirection(_input.MoveDirection);
+        Move();
+    }
+
+    public void Move()
+    {
+        Vector2 newPos = (Vector2)transform.position + _currentDirection.normalized * _currentSpeed * Time.deltaTime;
         transform.position = newPos;
     }
 
-    public Vector2 SetDirection()
+    public void SetDirection(Vector2 direction)
     {
-        throw new System.NotImplementedException();
+        _currentDirection = direction;
     }
 }
--- Assets/2.Script/Interface/IMovable.cs
+++ Assets/2.Script/Interface/IMovable.cs
@@ -4,8 +4,8 @@ public interface IMovable
 {
     public float CurrentSpeed { get; }
     //다음 프레임에 움질일 방향 정의
-    public Vector2 SetDirection();
+    public void SetDirection(Vector2 direction);
 
     //업데이트에서 사용하는 Move
-    public void Move(Vector2 direction);
+    public void Move();
 }
